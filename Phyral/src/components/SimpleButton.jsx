b91cb6b3db1182e464a7ad92810c4f5cba0bc5c5
Jonathan <143845545+corralesjonathan@users.noreply.github.com>
2024-05-03 13:34:32 -0600

    Changing favicon and adding features to simple button

--- Phyral/src/components/SimpleButton.jsx
+++ Phyral/src/components/SimpleButton.jsx
@@ -4,7 +4,7 @@ import '../index.css';
 export function SimpleButton({ href, name }) {
     return (
         <>
-            <a className='font-main font-semibold text-white bg-custom-blue px-10 py-4 w-fit rounded-md hover:bg-[#7F8BFF] transition-all' href={href}>{name}</a>
+            <a className='font-main font-semibold text-white bg-custom-blue px-10 py-4 w-fit rounded-md hover:opacity-90 duration-300 transition-all shadow-[0_8px_28px_0_rgba(81,97,255,0.35)]' href={href}>{name}</a>
         </>
     )
 
