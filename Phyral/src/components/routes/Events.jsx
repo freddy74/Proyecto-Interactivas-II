6313f360b04d2b006c0d4da54c9263b36e2e38dd
freddy74 <freddygarro74@gmail.com>
2024-05-27 01:38:28 -0600

    profile and sidebar adjustment

--- Phyral/src/components/routes/Events.jsx
+++ Phyral/src/components/routes/Events.jsx
@@ -7,11 +7,11 @@ import "../../index.css";
 
 export function Events() {
   return (
-    <div className="flex justify-end">
+    <>
       <Sidebar username="Freddy Garro" />
-      <section className="flex w-[80vw] max-sm:w-[100vw] max-md:w-[100vw] max-lg:w-[100vw] md:p-[40px] p-[20px] gap-[20px] mt-[80px]">
+      <section className="text-custom-dark grid mx-4 lg:ml-[20rem]">
         <div className="flex flex-col gap-[20px]">
-          <h1 className="text-custom-dark font-secondary font-black text-[40px]">
+          <h1 className="text-custom-dark font-secondary font-black text-[40px] max-lg:mt-28">
             My events
           </h1>
           <div className="flex justify-between items-center">
@@ -63,6 +63,6 @@ export function Events() {
           </div>
         </div>
       </section>
-    </div>
+    </>
   );
 }
